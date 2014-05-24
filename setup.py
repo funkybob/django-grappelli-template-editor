77e1d09221dd4f8c8fe238e6eade5329e3859fa6
funkybob <curtis@tinbrain.net>
2014-05-25 09:38:20 +1000

    Fixed packaging

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-grappelli-template-editor',
-    version='0.1',
+    version='0.2',
     description='A TTW Template Editor for Grappelli',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
     url='http://github.com/funkybob/django-grappelli-template-editor',
     keywords=['django', 'grappelli',],
     packages = find_packages(),
+    include_package_data=True,
     zip_safe=False,
     classifiers = [
         'Environment :: Web Environment',
